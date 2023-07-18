9541a6165d3a3b9cd9ee24d287125f503413eaf5
EyllGen <Eyllgen.off@gmail.com>
2023-07-18 13:42:52 -0700

    yees

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,12 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
  * main - check the code for alx project.
  *
  * Return: Always 0.
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+
+int _putchar(char c);
+
+#endif
