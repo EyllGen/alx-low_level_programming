d316d0593f0b71d24a76d7a4de4fcd585d046177
EyllGen <Eyllgen.off@gmail.com>
2023-07-18 13:14:06 -0700

    this is not good

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,14 +1,14 @@
+#include "holberton.h"
 #include <unistd.h>
-#include "main.h"
 
 /**
- * _putchar - write the characterv c to stdout 
- @c: the caracter to print 
-
- return: on success 1.
-          on error , -1 is returnrd. and errno is set appropriately.
-*/
-int _putchar(char c);
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-    return (write(1,&c, 1));
+	return (write(1, &c, 1));
 }
