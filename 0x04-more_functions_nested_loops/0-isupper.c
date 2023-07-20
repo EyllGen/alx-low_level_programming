28af9675ac8459afaf7ebecf6574aae68b73b338
EyllGen <Eyllgen.off@gmail.com>
2023-07-20 14:18:02 -0700

    last msg today

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * _isupper - uppercase letters
+ * @c: char to check
  *
- * Return: Always 0.
+ * Return: 0 or 1
  */
 
 int _isupper(int c)
@@ -13,3 +14,4 @@ int _isupper(int c)
 	else
 		return (0);
 }
+
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -6,6 +6,8 @@
  * Return: addition of the multiplicattion
  */
 
-int (a, b)
+int mul(int a, int b)
+{
+	return (a * b);
+}
 
-return( a* b)
