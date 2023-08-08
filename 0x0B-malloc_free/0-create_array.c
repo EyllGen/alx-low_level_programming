72174eea91dedabe94498f769e6ba662907c6117
EyllGen <Eyllgen.off@gmail.com>
2023-08-08 14:04:20 -0700

    commit 2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,18 +1,16 @@
-#include "main.h"
+[200~#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
-    unsigned int i;
-    char *ptr= malloc ((sizeof(char)) * size);
-    
-    if (size == 0);
-    return(NULL);
-    
-   
-	for (i = 0; i < size; i++)
-		*ptr[i] = c;
-    return(*ptr);
-    
+		char *ptr = malloc(sizeof(char) * size);
+			unsigned int i;
+
+				if (size == 0 || ptr == NULL)
+							return (NULL);
+
+					for (i = 0; i < size; i++)
+								ptr[i] = c;
+						return (ptr);
 }
